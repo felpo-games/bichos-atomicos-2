cd994fd785a69ee68fa410b111af55c060f15a5a
felpo-games <felipe.v.lima2006@gmail.com>
2026-04-24 20:30:55 -0300

    add20302404
    agora temos um campo mais organizado, e eletrizidade(não me lembro se adicionei)

---
+++ cenas/obstaculos/fogo_eletrico.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	show()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	pass
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if area.is_in_group("ataque_dc"):
+		hide()
+	pass # Replace with function body.
