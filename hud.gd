0911987441fe1032251f8f1a510bb0e9629ffdd2
Aitsuki-artesy <lvitoria10@gmail.com>
2026-05-19 19:06:39 -0300

    MENU SELETOR
    Teoricamente está funcional, porém não sei testar. Preciso de alguém que já tenha a síntese funcionando para validar o composto.

--- hud.gd
+++ hud.gd
@@ -1,6 +1,5 @@
 extends Control
 
-
 enum index_animation {expandir_1, expandir_2, expandir_3, encolher_1, encolher_2, encolher_3}
 var pending_exit: bool = false
 
@@ -13,7 +12,14 @@ func expan_enco(index: index_animation):
 	var anim_nomes = index_animation.keys()[index].to_lower()
 	anim_play.play(anim_nomes)
 	timer.start(0.5)
-
+	
+func incluir_bicho_img():
+	if laboratorio_global.pet_agua == true:
+		$CanvasLayer/Seletor2/Bicho1.texture = load("res://arte/vlad/satanas atomico/satanas atomico/nao sei oq atomico.png")
+	elif laboratorio_global.pet_dc == true:
+		$CanvasLayer/Seletor2/Bicho2.texture = load("res://arte/vlad/satanas atomico/satanas atomico/co2.png")
+	else:
+		pass
 func _on_seletor_1_mouse_entered() -> void:
 	expan_enco(index_animation.expandir_1)
 
