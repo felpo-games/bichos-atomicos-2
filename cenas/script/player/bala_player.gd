ae7acb2fa273fa45c9e6380d85f588b7a2e9da42
felpo-games <felipe.v.lima2006@gmail.com>
2026-05-19 22:55:31 -0300

    add disparo esta caindo

--- cenas/script/player/bala_player.gd
+++ cenas/script/player/bala_player.gd
@@ -1,27 +1,55 @@
 extends Area3D
 
-@export var velocidade = 10.0
-@export var dano = 1
+@export var velocidade := 40.0
+@export var dano := 1
+@export var gravidade := 20.0 # Força da gravidade que puxa para baixo
+
+var direcao: Vector3 = Vector3.ZERO
+var velocidade_vertical := 0.0 # Controla a subida e a descida
 
 func _ready() -> void:
-	await get_tree().create_timer(5.0).timeout
+	# Define uma velocidade vertical inicial para fazê-la começar subindo
+	# Ajuste esse valor (ex: 10.0) para controlar a altura do arco
+	velocidade_vertical = 12.0 
+	
+	# destrói depois de 2 segundos
+	await get_tree().create_timer(2.0).timeout
 	queue_free()
 
-func _process(delta: float) -> void:
-	translate(Vector3(0, 0, -velocidade * delta))
+func _physics_process(delta: float) -> void:
+	# 1. Movimento horizontal (X e Z) baseado na direção desejada
+	var movimento_horizontal = direcao.normalized() * velocidade * delta
+	global_position.x += movimento_horizontal.x
+	global_position.z += movimento_horizontal.z
+	
+	# 2. Aplica a gravidade na velocidade vertical ao longo do tempo
+	velocidade_vertical -= gravidade * delta
+	
+	# 3. Aplica a velocidade vertical na posição Y do projétil
+	global_position.y += velocidade_vertical * delta
+
 
 func _on_body_entered(body: Node3D) -> void:
 	if !body.is_in_group("player"):
 		destruir_com_som()
 
+
 func _on_area_entered(area: Area3D) -> void:
 	if area.is_in_group("bicho"):
 		destruir_com_som()
 
+
 func destruir_com_som() -> void:
-	velocidade = 0 
+
 	$frasco.hide()
+
 	$CollisionShape3D.set_deferred("disabled", true)
+
 	$SfxImpacto.play()
+
 	await $SfxImpacto.finished
+
 	queue_free()
+
+func caindo():
+	global_position.y -= 1
--- cenas/script/player/player.gd
+++ cenas/script/player/player.gd
@@ -4,8 +4,11 @@ var em_dialogo: bool = false
 
 @onready var menu_opcoes: Control = $"../../UI/telas/Config"
 @export var ui_atomo: Control
+
+# VIDA
 var vida = 3
 
+# ESTADOS
 enum estado {normal, batalhando, morto}
 var state = estado.normal
 
@@ -13,115 +16,149 @@ var state = estado.normal
 var SPEED = 15.0
 @export var rotation_speed = 10.0
 
-@export var bala_cena:PackedScene
+# TIRO
+@export var bala_cena: PackedScene
 @onready var arma: Marker3D = $body/Marker3D
 
+var pode_atirar = true
+@export var tempo_tiro := 0.3
+
+# GRAVIDADE
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-# DASH
-var is_dashing := false
-var dash_timer := 0.5
+# INVENCIBILIDADE
+var tomar_dano = true
+@export var tempo_dano := 1.0
 
-@export var dash_speed := 100
-var dash_speed_normal := 100
-@export var dash_duration := 0.15
-
-var dash_em_cooldown := false
-@export var dash_cooldown := 1.0
-
-# ✅ KNOCKBACK
+# KNOCKBACK
 var em_knockback := false
 @export var tempo_knockback := 0.3
+
+
+
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("abri_tela_opcoes"):
 		alternar_pausa()
+
 
 func alternar_pausa():
 	var jogo_pausado = get_tree().paused
 	get_tree().paused = !jogo_pausado
 	menu_opcoes.visible = !jogo_pausado
 
-func _ready() -> void:
-	eventos_global.batalha = false
-
 func _physics_process(delta: float) -> void:
-	
+
+	# troca pet
 	if Input.is_action_just_pressed("troca_de_pet"):
 		laboratorio_global.pet_1 = !laboratorio_global.pet_1
 		print(laboratorio_global.pet_1)
-	
+
 	# tiro
 	if Input.is_action_just_pressed("campirar") and eventos_global.batalha == true:
 		atirar()
+		print(atirar)
+		
 
-	if eventos_global.numa_tela == false:
-		if not is_on_floor() and not is_dashing:
-			velocity.y -= gravity * delta
-
+	# dialogo
 	if em_dialogo:
+
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+
 		$body/pivod/AnimationPlayer.play("idle_PL")
+
 		move_and_slide()
 		return
 
+	# gravidade
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# movimento
 	if not em_knockback:
-		
+
 		var input_dir := Input.get_vector("esquerda", "direita", "frente", "tras")
+
 		var direction := Vector3(input_dir.x, 0, input_dir.y).normalized()
 
-		if is_dashing:
-			dash_timer -= delta
-			if dash_timer <= 0:
-				is_dashing = false
+		if direction:
+
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+
+			$body/pivod/AnimationPlayer.play("walk_Pl")
+
+			var target_angle = atan2(direction.x, direction.z)
+
+			$body.rotation.y = lerp_angle(
+				$body.rotation.y,
+				target_angle,
+				rotation_speed * delta
+			)
+
 		else:
-			if direction:
-				velocity.x = direction.x * SPEED
-				velocity.z = direction.z * SPEED
-				$body/pivod/AnimationPlayer.play("walk_Pl")
-				var target_angle = atan2(direction.x, direction.z)
-				$body.rotation.y = lerp_angle($body.rotation.y, target_angle, rotation_speed * delta)
-			else:
-				$body/pivod/AnimationPlayer.stop()
-				velocity.x = move_toward(velocity.x, 0, SPEED)
-				velocity.z = move_toward(velocity.z, 0, SPEED)
 
-		move_and_slide()
+			$body/pivod/AnimationPlayer.play("idle_PL")
 
-func receber_knockback(forca: Vector3):
-	em_knockback = true
-	
-	# cancela dash se estiver acontecendo
-	is_dashing = false
-	
-	velocity = forca
-	
-	await get_tree().create_timer(tempo_knockback).timeout
-	em_knockback = false
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
 
 func atirar():
+	if not eventos_global.batalha:
+		return
+
+	if not pode_atirar:
+		return
+
+	pode_atirar = false
+
 	var bala = bala_cena.instantiate()
+
 	get_tree().root.add_child(bala)
+
 	bala.global_position = arma.global_position
-	bala.global_rotation = arma.global_rotation
+
+	# direção do BODY que realmente gira
+	bala.direcao = $body.global_transform.basis.z.normalized()
+
 	$AUDIOS/SfxArremesso.play()
 
+	await get_tree().create_timer(tempo_tiro).timeout
+
+	pode_atirar = true
+
+
 func dano():
+	# invencibilidade
+	if not tomar_dano:
+		return
+	tomar_dano = false
 	if vida > 0:
 		vida -= 1
+		print("vida:", vida)
 		if ui_atomo:
 			ui_atomo.atualizar_vida(vida)
 		if vida <= 0:
 			AudioManager.get_node("SFXGameOver").play()
 			morrer()
+	# espera invencibilidade
+	await get_tree().create_timer(tempo_dano).timeout
+	tomar_dano = true
+
+func receber_knockback(forca: Vector3):
+	em_knockback = true
+	velocity = forca
+	await get_tree().create_timer(tempo_knockback).timeout
+	em_knockback = false
 
 func morrer():
+	state = estado.morto
 	get_tree().change_scene_to_file("res://cenas/gameover.tscn")
 
 func iniciar_batalha():
 	pass
-
-func _on_area_dano_area_entered(area: Area3D) -> void:
-	if area.is_in_group("ataque_bicho"):
-		dano()
