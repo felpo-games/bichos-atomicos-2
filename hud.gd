9fcd57187964fdcfcf58e87736fe624ebc8fe63b
Aitsuki-artesy <lvitoria10@gmail.com>
2026-05-19 21:07:52 -0300

    CÓDIGO COM SHOW/HIDE
    Mudei para simplificar o código, esperando que isso o faça funcionar.

--- hud.gd
+++ hud.gd
@@ -1,8 +1,9 @@
 extends Control
 
 enum index_animation {expandir_1, expandir_2, expandir_3, encolher_1, encolher_2, encolher_3}
-var pending_exit: bool = false
-const cena_elenmeyer = preload("res://cenas/telas/laboratorio_iventario.tscn")
+var pending_exit: bool
+
+@export var cena_elenmeyer: PackedScene
 
 @onready var timer = $Timer
 @onready var anim_play = $AnimationPlayer
@@ -15,15 +16,15 @@ func expan_enco(index: index_animation):
 	# para bater com o nome da animação no nó
 	var anim_nomes = index_animation.keys()[index].to_lower()
 	anim_play.play(anim_nomes)
+	pending_exit = true
 	timer.start(0.5)
 	
 func incluir_bicho_img():
 	if laboratorio_global.pet_agua == true:
-		$CanvasLayer/Seletor2/Bicho1.texture = load("res://arte/vlad/satanas atomico/satanas atomico/nao sei oq atomico.png")
-	elif laboratorio_global.pet_dc == true:
-		$CanvasLayer/Seletor2/Bicho2.texture = load("res://arte/vlad/satanas atomico/satanas atomico/co2.png")
-	else:
-		pass
+		$CanvasLayer/Seletor1/Bicho1.show()
+	if laboratorio_global.pet_dc == true:
+		$CanvasLayer/Seletor2/Bicho2.show()
+	pass
 func _on_seletor_1_mouse_entered() -> void:
 	expan_enco(index_animation.expandir_1)
 
@@ -43,6 +44,11 @@ func _on_seletor_3_mouse_exited() -> void:
 	expan_enco(index_animation.encolher_3)
 	
 func _on_timer_timeout() -> void:
-	pending_exit = pending_exit
+	pending_exit = false
+	timer.stop()
 	print("timeout ok")
+	pass # Replace with function body.
+
+func _on_button_pressed() -> void:
+	abrir_elenmeyer()
 	pass # Replace with function body.
